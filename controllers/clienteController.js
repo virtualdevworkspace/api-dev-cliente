119daa4f8dd9a5d1d11621013e6b779fa918c162
Jose Paulino Perez Godinez <virtualdevworkspace@gmail.com>
2024-08-31 08:24:49 -0600

    correccion de arreglo...

--- controllers/clienteController.js
+++ controllers/clienteController.js
@@ -1,21 +1,11 @@
-// controllers/clienteController.js
 const clienteModel = require('../models/clienteModel');
 
 const createCliente = async (req, res) => {
   try {
-    const clientes = req.body; // Suponiendo que `req.body` es un array de clientes
-    
-    if (!Array.isArray(clientes) || clientes.length === 0) {
-      return res.status(400).json({ error: 'El cuerpo de la solicitud debe ser un array no vacío de clientes' });
-    }
-
-    // Puedes agregar validaciones adicionales aquí si es necesario
-
-    const result = await clienteModel.createClientes(clientes);
-    res.status(201).json(result);
+    const cliente = await clienteModel.createCliente(req.body);
+    res.status(201).json(cliente);
   } catch (error) {
-    console.error('Error al insertar clientes:', error.message);
-    res.status(500).json({ error: 'Error al insertar clientes' });
+    res.status(500).json({ error: 'Error al crear cliente' });
   }
 };
 
@@ -24,7 +14,6 @@ const getClientes = async (req, res) => {
     const clientes = await clienteModel.getClientes();
     res.status(200).json(clientes);
   } catch (error) {
-    console.error('Error al obtener clientes:', error.message);
     res.status(500).json({ error: 'Error al obtener clientes' });
   }
 };
@@ -38,7 +27,6 @@ const getClienteById = async (req, res) => {
       res.status(404).json({ error: 'Cliente no encontrado' });
     }
   } catch (error) {
-    console.error('Error al obtener cliente:', error.message);
     res.status(500).json({ error: 'Error al obtener cliente' });
   }
 };
@@ -52,7 +40,6 @@ const updateCliente = async (req, res) => {
       res.status(404).json({ error: 'Cliente no encontrado' });
     }
   } catch (error) {
-    console.error('Error al actualizar cliente:', error.message);
     res.status(500).json({ error: 'Error al actualizar cliente' });
   }
 };
@@ -66,7 +53,6 @@ const deleteCliente = async (req, res) => {
       res.status(404).json({ error: 'Cliente no encontrado' });
     }
   } catch (error) {
-    console.error('Error al eliminar cliente:', error.message);
     res.status(500).json({ error: 'Error al eliminar cliente' });
   }
 };
